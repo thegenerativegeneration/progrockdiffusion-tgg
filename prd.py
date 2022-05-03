337db151983b39ef7c0ad0acef103f813c51c753
Jason Hough <jason@jasonhough.com>
2022-05-02 21:21:02 -0700

    Improved model download logic, added fallback URLs

--- prd.py
+++ prd.py
@@ -2076,91 +2076,125 @@ other_sampling_mode = 'bicubic'
 #@markdown If you're having issues with model downloads, check this to compare SHA's:
 check_model_SHA = False  #@param{type:"boolean"}
 
-model_256_SHA = '983e3de6f95c88c81b2ca7ebb2c217933be1973b1ff058776b970f901584613a'
-model_512_SHA = '9c111ab89e214862b76e1fa6a1b3f1d329b1a88281885943d2cdbe357ad57648'
-model_secondary_SHA = '983e3de6f95c88c81b2ca7ebb2c217933be1973b1ff058776b970f901584613a'
+def download_models(diffusion_model,use_secondary_model,fallback=False):
+  model_256_downloaded = False
+  model_512_downloaded = False
+  model_secondary_downloaded = False
 
-model_256_link = 'https://openaipublic.blob.core.windows.net/diffusion/jul-2021/256x256_diffusion_uncond.pt'
-model_512_link = 'http://batbot.tv/ai/models/guided-diffusion/512x512_diffusion_uncond_finetune_008100.pt'
-model_secondary_link = 'https://the-eye.eu/public/AI/models/v-diffusion/secondary_model_imagenet_2.pth'
+  model_256_SHA = '983e3de6f95c88c81b2ca7ebb2c217933be1973b1ff058776b970f901584613a'
+  model_512_SHA = '9c111ab89e214862b76e1fa6a1b3f1d329b1a88281885943d2cdbe357ad57648'
+  model_secondary_SHA = '983e3de6f95c88c81b2ca7ebb2c217933be1973b1ff058776b970f901584613a'
 
-model_256_path = f'{model_path}/256x256_diffusion_uncond.pt'
-model_512_path = f'{model_path}/512x512_diffusion_uncond_finetune_008100.pt'
-model_secondary_path = f'{model_path}/secondary_model_imagenet_2.pth'
+  model_256_link = 'https://openaipublic.blob.core.windows.net/diffusion/jul-2021/256x256_diffusion_uncond.pt'
+  model_512_link = 'http://batbot.tv/ai/models/guided-diffusion/512x512_diffusion_uncond_finetune_008100.pt'
+  model_secondary_link = 'https://the-eye.eu/public/AI/models/v-diffusion/secondary_model_imagenet_2.pth'
 
-# Download the diffusion model
-if diffusion_model == '256x256_diffusion_uncond':
+  model_256_link_fb = 'https://www.dropbox.com/s/9tqnqo930mpnpcn/256x256_diffusion_uncond.pt'
+  model_512_link_fb = 'https://www.dropbox.com/s/yjqvhu6l6l0r2eh/512x512_diffusion_uncond_finetune_008100.pt'
+  model_secondary_link_fb = 'https://www.dropbox.com/s/luv4fezod3r8d2n/secondary_model_imagenet_2.pth'
+
+  model_256_path = f'{model_path}/256x256_diffusion_uncond.pt'
+  model_512_path = f'{model_path}/512x512_diffusion_uncond_finetune_008100.pt'
+  model_secondary_path = f'{model_path}/secondary_model_imagenet_2.pth'
+
+  if fallback:
+    model_256_link = model_256_link_fb
+    model_512_link = model_512_link_fb
+    model_secondary_link = model_secondary_link_fb
+  # Download the diffusion model
+  if diffusion_model == '256x256_diffusion_uncond':
     if os.path.exists(model_256_path) and check_model_SHA:
-        print('Checking 256 Diffusion File')
-        with open(model_256_path, "rb") as f:
-            bytes = f.read()
-            hash = hashlib.sha256(bytes).hexdigest()
-        if hash == model_256_SHA:
-            print('256 Model SHA matches')
-            model_256_downloaded = True
-        else:
-            print("256 Model SHA doesn't match, redownloading...")
-            #!wget --continue {model_256_link} -P {model_path}
-            urllib.request.urlretrieve(model_256_link, model_256_path)
-            model_256_downloaded = True
-    elif os.path.exists(
-            model_256_path
-    ) and not check_model_SHA or model_256_downloaded == True:
-        pass
-    else:
-        print("256 Model downloading, this might take a while...")
-        #!wget --continue {model_256_link} -P {model_path}
-        urllib.request.urlretrieve(model_256_link, model_256_path)
+      print('Checking 256 Diffusion File')
+      with open(model_256_path,"rb") as f:
+          bytes = f.read()
+          hash = hashlib.sha256(bytes).hexdigest();
+      if hash == model_256_SHA:
+        print('256 Model SHA matches')
         model_256_downloaded = True
-elif diffusion_model == '512x512_diffusion_uncond_finetune_008100':
+      else:
+        print("256 Model SHA doesn't match, redownloading...")
+        #wget(model_256_link, model_path)
+        print('256 Model downloading. This may take a while...')
+        urllib.request.urlretrieve(model_256_link, model_256_path)
+        if os.path.exists(model_256_path):
+          model_256_downloaded = True
+        else:
+          print('First URL Failed using FallBack')
+          download_models(diffusion_model,use_secondary_model,True)
+    elif os.path.exists(model_256_path) and not check_model_SHA or model_256_downloaded == True:
+      print('256 Model already downloaded, check check_model_SHA if the file is corrupt')
+    else:
+      #wget(model_256_link, model_path)
+      print('256 Model downloading. This may take a while...')
+      urllib.request.urlretrieve(model_256_link, model_256_path)
+      if os.path.exists(model_256_path):
+        model_256_downloaded = True
+      else:
+        print('First URL failed, using backup')
+        download_models(diffusion_model,True)
+  elif diffusion_model == '512x512_diffusion_uncond_finetune_008100':
     if os.path.exists(model_512_path) and check_model_SHA:
-        print('Checking 512 Diffusion File')
-        with open(model_512_path, "rb") as f:
-            bytes = f.read()
-            hash = hashlib.sha256(bytes).hexdigest()
-        if hash == model_512_SHA:
-            print('512 Model SHA matches')
-            model_512_downloaded = True
+      print('Checking 512 Diffusion File')
+      with open(model_512_path,"rb") as f:
+          bytes = f.read()
+          hash = hashlib.sha256(bytes).hexdigest();
+      if hash == model_512_SHA:
+        print('512 Model SHA matches')
+        if os.path.exists(model_512_path):
+          model_512_downloaded = True
         else:
-            print("512 Model SHA doesn't match, redownloading...")
-            #!wget --continue {model_512_link} -P {model_path}
-            urllib.request.urlretrieve(model_512_link, model_512_path)
-            model_512_downloaded = True
-    elif os.path.exists(
-            model_512_path
-    ) and not check_model_SHA or model_512_downloaded == True:
-        pass
-    else:
-        #!wget --continue {model_512_link} -P {model_path}
-        print(f"512 Model downloading to {model_path}, this might take a while...")
+          print('First URL failed, using backup')
+          download_models(diffusion_model,use_secondary_model,True)
+      else:
+        print("512 Model SHA doesn't match, redownloading...")
+        #wget(model_512_link, model_path)
+        print('512 Model downloading. This may take a while...')
         urllib.request.urlretrieve(model_512_link, model_512_path)
-        model_512_downloaded = True
-
-# Download the secondary diffusion model v2
-if use_secondary_model == True:
-    if os.path.exists(model_secondary_path) and check_model_SHA:
-        print('Checking Secondary Diffusion File')
-        with open(model_secondary_path, "rb") as f:
-            bytes = f.read()
-            hash = hashlib.sha256(bytes).hexdigest()
-        if hash == model_secondary_SHA:
-            print('Secondary Model SHA matches')
-            model_secondary_downloaded = True
+        if os.path.exists(model_512_path):
+          model_512_downloaded = True
         else:
-            print("Secondary Model SHA doesn't match, redownloading...")
-            #!wget --continue {model_secondary_link} -P {model_path}
-            urllib.request.urlretrieve(model_secondary_link,
-                                       model_secondary_path)
-            model_secondary_downloaded = True
-    elif os.path.exists(
-            model_secondary_path
-    ) and not check_model_SHA or model_secondary_downloaded == True:
-        pass
+          print('First URL failed, using backup')
+          download_models(diffusion_model,use_secondary_model,True)
+    elif os.path.exists(model_512_path) and not check_model_SHA or model_512_downloaded == True:
+      print('512 Model already downloaded, check check_model_SHA if the file is corrupt')
     else:
-        #!wget --continue {model_secondary_link} -P {model_path}
-        print('Secondary Model downloading, this might take a while...')
-        urllib.request.urlretrieve(model_secondary_link, model_secondary_path)
+      #wget(model_512_link, model_path)
+      print('512 Model downloading. This may take a while...')
+      urllib.request.urlretrieve(model_512_link, model_512_path)
+      model_512_downloaded = True
+  # Download the secondary diffusion model v2
+  if use_secondary_model == True:
+    if os.path.exists(model_secondary_path) and check_model_SHA:
+      print('Checking Secondary Diffusion File')
+      with open(model_secondary_path,"rb") as f:
+          bytes = f.read()
+          hash = hashlib.sha256(bytes).hexdigest();
+      if hash == model_secondary_SHA:
+        print('Secondary Model SHA matches')
         model_secondary_downloaded = True
+      else:
+        print("Secondary Model SHA doesn't match, redownloading...")
+        #wget(model_secondary_link, model_path)
+        print('Secondary Model downloading. This may take a while...')
+        urllib.request.urlretrieve(model_secondary_link, model_secondary_path)
+        if os.path.exists(model_secondary_path):
+          model_secondary_downloaded = True
+        else:
+          print('First URL failed, using backup')
+          download_models(diffusion_model,use_secondary_model,True)
+    elif os.path.exists(model_secondary_path) and not check_model_SHA or model_secondary_downloaded == True:
+      print('Secondary Model already downloaded, check check_model_SHA if the file is corrupt')
+    else:
+      #wget(model_secondary_link, model_path)
+      print('Secondary Model downloading. This may take a while...')
+      urllib.request.urlretrieve(model_secondary_link, model_secondary_path)
+      if os.path.exists(model_secondary_path):
+          model_secondary_downloaded = True
+      else:
+        print('First URL Failed using FallBack')
+        download_models(diffusion_model,use_secondary_model,True)
+
+download_models(diffusion_model,use_secondary_model)
 
 model_config = model_and_diffusion_defaults()
 if diffusion_model == '512x512_diffusion_uncond_finetune_008100':
